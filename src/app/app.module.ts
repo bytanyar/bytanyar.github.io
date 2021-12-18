79d0ac226f2e23822d9db90c2873f1110abfbb49
Tanya Robertson <bytanyar@yahoo.com>
2021-12-17 19:16:56 -0500

    copying old files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './services/token-interceptor.service';
+
 import { AppComponent } from './app.component';
+import { DataComponent } from './knowledge/knowledge.component';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CatsComponent } from './cats/cats.component';
+
+import { ApiCatService } from './services/cats.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent,
+    HomeComponent,
+    ExperienceComponent,
+    CatsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiCatService,
+              { provide: HTTP_INTERCEPTORS, 
+                useClass: TokenInterceptor, 
+                multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
